fe7772ccf7849dc0911bf68a4be7bacf4b334951
Nash Pherson <nashp@nowmicro.com>
2018-02-12 14:59:45 -0600

    Fixed the event ID
    @Bdam555 pointed out we had the wrong event ID copied in here. We didn't have an actual group policy broken system to demo with, so we picked a different error, but forgot to update the script before upload.

--- MMS Broken Group Policy.ps1
+++ MMS Broken Group Policy.ps1
@@ -1,3 +1,3 @@
-
+# EventID 1096 is logged when a system cannot update group policy, typically because the registry.pol file is corrupt.
 powershell.exe -Version 2
-Get-EventLog -LogName System -Source Microsoft-Windows-GroupPolicy -EntryType Error | Where-Object($_.EventID -eq 10016)
+Get-EventLog -LogName System -Source Microsoft-Windows-GroupPolicy -EntryType Error | Where-Object($_.EventID -eq 1096)
