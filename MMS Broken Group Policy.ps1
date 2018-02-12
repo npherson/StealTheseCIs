460152558089fad8f40c00bfefc97d880e81bdbc
Nash Pherson <nashp@nowmicro.com>
2018-02-12 15:26:50 -0600

    Removed PowerShell V2
    After testing, seems to work the same on v2 and vCurrent. Removing step for forcing v2.

--- MMS Broken Group Policy.ps1
+++ MMS Broken Group Policy.ps1
@@ -1,3 +1,2 @@
 # EventID 1096 is logged when a system cannot update group policy, typically because the registry.pol file is corrupt.
-powershell.exe -Version 2
 Get-EventLog -LogName System -Source Microsoft-Windows-GroupPolicy -EntryType Error | Where-Object($_.EventID -eq 1096)
